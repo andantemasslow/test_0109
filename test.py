ee0c9d2f854a1d1fbf6417345d5f07ceb60df2ae
yerihyo <yerihyo@gmail.com>
2019-01-08 19:09:25 -0800

    some change by moon

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 26 2018, 23:26:24) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> a=3
->>> b=4
->>> print(a+b)
-7
->>> 
+#!/usr/bin/env python
+
+def func(n):
+    return n+1
+
+def main():
+    a = 3
+    b = 5
+    print("this: {0}".format(a+b))
+
+    z = func(5)
+    print(z)
+
+if __name__ == "__main__":
+    main()
